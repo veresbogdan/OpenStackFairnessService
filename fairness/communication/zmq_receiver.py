dd69e53a300bf8857eb825ec6d63830afd945efc
Bogdan <veresbogdan91@gmail.com>
2016-11-01 15:51:44 +0100

    temp change

--- fairness/communication/zmq_receiver.py
+++ fairness/communication/zmq_receiver.py
@@ -44,7 +44,7 @@ class Receiver:
                 if key.__contains__('nri'):
                     print "got Nri"
                     if self.nri_sent < 2:
-                        self.nri.server_nris = json_msj
+                        self.nri.server_nris[key] = json_msj[key]
                         sender.send_nri(self.nri)
                         Receiver.nri_sent += 1
 
