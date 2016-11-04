0309dcf6d9af75c3a642b4f331e8a716e4d99fbb
rpatan <riccardo.patane@uzh.ch>
2016-11-04 18:38:47 +0100

    test

--- fairness/openstack_driver.py
+++ fairness/openstack_driver.py
@@ -48,7 +48,6 @@ class IdentityApiConnection(object):
         r = requests.get(url, headers=headers)
         print r.text
         print r.status_code
-        # json_data = json.dumps(r.text)
-        # item_dict = json.loads(json_data)
-        print r.text["users"]
+        json_text = json.loads(r.text)
+        print json_text["users"]
 
