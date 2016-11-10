39d93ba8eaa8eaa551432915c2508885c70f421b
Bogdan <veresbogdan91@gmail.com>
2016-11-10 11:30:57 +0100

    Test greediness messaging

--- fairness/communication/zmq_sender.py
+++ fairness/communication/zmq_sender.py
@@ -28,8 +28,7 @@ class Sender:
 
         json_string = json.dumps(nri.server_nris)
         self.socket.send(json_string)
-        reply = Sender.socket.recv()
-        print reply
+        Sender.socket.recv()
 
     def send_greediness(self, rui):
         key = 'greed_' + NRI._get_public_ip_address()
@@ -41,12 +40,13 @@ class Sender:
         else:
             # if not any(d.get(key, None) is not None for d in nri.server_nris['nri']):
             #     rui.server_greediness['greed'][key] = own_greed
+            print 'enter else '
             lool = [own_greed if key in x else x for x in rui.server_greediness['greed']]
+            print 'the lool: '
             print lool
             rui.server_greediness['greed'] = lool
 
         json_string = json.dumps(rui.server_greediness)
         self.socket.send(json_string)
-        reply = Sender.socket.recv()
-        print reply
+        Sender.socket.recv()
 
