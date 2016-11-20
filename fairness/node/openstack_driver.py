d8ea43cc8994f3848096a9884b3fb6b051eef83d
rpatan <riccardo.patane@uzh.ch>
2016-11-20 11:37:09 +0100

    Quotas API test

--- fairness/node/openstack_driver.py
+++ fairness/node/openstack_driver.py
@@ -66,7 +66,7 @@ class IdentityApiConnection(object):
             json_text = json.loads(r.text)
             self.token_exp = json_text['token']['expires_at']
             self.token_issued = json_text['token']['issued_at']
-            print "Length: " len(json_text['token']['catalog'])
+            print "Length: ", len(json_text['token']['catalog'])
             self.project_id = json_text['token']['catalog']['']
 
     def _check_token(self):
