7f26fd1d99b1bd8111ed7522527b9bfceb7322bf
rpatan <riccardo.patane@uzh.ch>
2016-11-21 17:20:59 +0100

    zmq test

--- fairness/for_tests.py
+++ fairness/for_tests.py
@@ -6,7 +6,7 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
 print("Collecting updates...")
-socket.connect("tcp://192.168.100.102:5556")
+socket.connect("tcp://localhost:5556")
 
 # Subscribe to zipcode, default is NYC, 10001
 zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
