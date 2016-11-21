26ea0aba1c9cf0ce60ab4c0f030f3b8c60b32355
rpatan <riccardo.patane@uzh.ch>
2016-11-21 17:11:27 +0100

    zmq test

--- fairness/for_tests_2.py
+++ fairness/for_tests_2.py
@@ -3,7 +3,7 @@ from random import randrange
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:5556")
+socket.bind("tcp://192.168.100.101:5556")
 
 while True:
     zipcode = randrange(1, 100000)
