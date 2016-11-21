fed9c7f7096ec9ec01cc8fc55c91658d601e7106
rpatan <riccardo.patane@uzh.ch>
2016-11-21 16:54:41 +0100

    zmq test

--- fairness/for_tests.py
+++ fairness/for_tests.py
@@ -1,69 +1,28 @@
+import sys
+import zmq
 
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
 
-# import subprocess
-# import os
-#
-#
-# def _get_disks():
-#     disks = list()
-#     try:
-#         output = subprocess.check_output(['lsblk', '-io', 'KNAME,TYPE'])
-#         if output is not None:
-#             for line in output.splitlines():
-#                 line_segments = line.split(' ')
-#                 if line_segments[len(line_segments) - 1] == 'disk':
-#                     disks.append(line_segments[0])
-#     except subprocess.CalledProcessError:
-#         print ("An error in _get_disks() has ocured: Command 'exit 1' returned non-zero exit status 1")
-#     return disks
-#
-#
-# def _get_disk_write_speeds():
-#     iterations = 1
-#     speeds = 0
-#     disks = _get_disks()
-#     try:
-#         for disk in disks:
-#             inner_sum_speed = 0
-#             err = None
-#             for i in range(iterations):
-#                 output, err = subprocess.Popen(['sudo', 'dd', 'if=/dev/' + disk, 'of=' + os.path.expanduser('~/disk_benchmark_file'), 'bs=8k', 'count=200k'], stderr=subprocess.PIPE).communicate()
-#                 subprocess.call(['sudo', 'rm', os.path.expanduser('~/disk_benchmark_file')])
-#                 print err
-#                 if err is not None:
-#                     lines = err.splitlines()
-#                     line_segments = lines[2].split(' ')
-#                     speed_in_mbs = line_segments[len(line_segments) - 2]
-#                     speed_in_bytes = float(speed_in_mbs) * 1000000
-#                     inner_sum_speed += int(speed_in_bytes)
-#             if err is not None:
-#                 speeds += inner_sum_speed / iterations
-#     except subprocess.CalledProcessError:
-#         print ("An error in _get_disk_write_speeds() has ocured: Command 'exit 1' returned non-zero exit status 1")
-#     print speeds
-#
-# if __name__ == '__main__':
-#     _get_disk_write_speeds()
+print("Collecting updates...")
+socket.connect("tcp://localhost:5556")
 
+# Subscribe to zipcode, default is NYC, 10001
+zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
 
+# Python 2 - ascii bytes to unicode str
+if isinstance(zip_filter, bytes):
+    zip_filter = zip_filter.decode('ascii')
+socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
 
+# Process 5 updates
+total_temp = 0
+for update_nbr in range(5):
+    string = socket.recv_string()
+    zipcode, temperature, relhumidity = string.split()
+    total_temp += int(temperature)
 
-
-
-
-# import datetime
-#
-# time_test =datetime.datetime(2016, 11, 07, 19, 05, 31, 000001).isoformat()
-# time_now = datetime.datetime.now().isoformat()
-#
-# print "time test: ", time_test
-# print "time now:  ", time_now
-#
-# if time_test > time_now:
-#     print "bigger than"
-#     print "Exp: ", time_test
-#     print "Now: ", time_now
-# else:
-#     print "ELSE (not >)"
-#     print "Exp: ", time_test
-#     print "Now: ", time_now
+print("Average temperature for zipcode '%s' was %dF" % (
+      zip_filter, total_temp / (update_nbr+1))
+)
