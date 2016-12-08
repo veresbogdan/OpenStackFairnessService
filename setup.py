a408fe5481cd087ec06ebf0df8763e9f4eee6fc0
rpatan <riccardo.patane@uzh.ch>
2016-12-08 16:20:29 +0100

    change licence to Apache 2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0.dev1',
+    version='0.1.1.dev1',
 
     description='A service to enforce a fair resource utilization in a OpenStack infrastructure '
                 'by automatic and dynamic reallocation of the resources (cpu, mem, disk, network) during runtime.',
