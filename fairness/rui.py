5c4115d4787c2f2aeb39c86f7c0853db4a01e5a5
Riccardo Patane <riccardo.patane.ch@gmail.com>
2016-12-17 12:35:55 +0100

    add RUI difference

--- fairness/rui.py
+++ fairness/rui.py
@@ -11,20 +11,20 @@ class RUI(object):
     server_greediness = {}
 
     def __init__(self):
-        self.cpu_time = None
-        self.memory_used = None
-        self.disk_bytes_read = None
-        self.disk_bytes_written = None
-        self.network_bytes_received = None
-        self.network_bytes_transmitted = None
-        self.time_stamp = None
+        self.cpu_time = 0
+        self.memory_used = 0
+        self.disk_bytes_read = 0
+        self.disk_bytes_written = 0
+        self.network_bytes_received = 0
+        self.network_bytes_transmitted = 0
+        self.time_stamp = 0
 
     def get_utilization(self, domain_id):
         conn = LibvirtConnection()
-        self.cpu_time = conn.get_vcpu_stats(domain_id)
-        self.memory_used = conn.get_memory_stats(domain_id)
-        self.disk_bytes_read = conn.get_disk_stats(domain_id)[0]  # 2 for IOPS
-        self.disk_bytes_written = conn.get_disk_stats(domain_id)[1]  # 3 for IOPS
-        self.network_bytes_received = conn.get_network_stats(domain_id)[0]
-        self.network_bytes_transmitted = conn.get_network_stats(domain_id)[1]
+        self.cpu_time = conn.get_vcpu_stats(domain_id) - self.cpu_time
+        self.memory_used = conn.get_memory_stats(domain_id) - self.memory_used
+        self.disk_bytes_read = conn.get_disk_stats(domain_id)[0] - self.disk_bytes_read  # 2 for IOPS
+        self.disk_bytes_written = conn.get_disk_stats(domain_id)[1] - self.disk_bytes_written  # 3 for IOPS
+        self.network_bytes_received = conn.get_network_stats(domain_id)[0] - self.network_bytes_received
+        self.network_bytes_transmitted = conn.get_network_stats(domain_id)[1] - self.network_bytes_transmitted
         self.time_stamp = datetime.now().time()
