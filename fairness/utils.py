b577db80febfaeae70160c66e6c83302756222df
Bogdan <veresbogdan91@gmail.com>
2017-02-07 14:19:08 +0100

    Quick bug fix

--- fairness/utils.py
+++ fairness/utils.py
@@ -1,13 +1,16 @@
 from collections import defaultdict
 
+import numpy as np
+
 
 def dsum(*dicts):
     ret = defaultdict(int)
     for d in dicts:
         if type(d) is dict:
             for k, v in d.items():
-                if type(v) is list and v is not None:
-                    d = dsum(d, v)
+                if type(v) is list:
+                    # TODO sum up the vec?
+                    pass
                 else:
                     if v is not None:
                         ret[k] += v
